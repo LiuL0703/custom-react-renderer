d0e64b4fd1469df4ce7dd16b01bbe0f89cc931c4
liul0703 <louieliu0703@gmail.com>
2020-03-07 17:50:01 +0800

    perf:structure

--- src/App.js
+++ src/App.js
@@ -6,33 +6,35 @@ function App() {
   const [count, setCount] = useState(0);
   const [showLogo, setShowLogo] = useState(true);
   return (
-    <div 
-      className="App" 
-      onClick={()=> {
-        console.log('----')
-        setShowLogo(show => !show)
-      }}
-    >
-      <header className="App-header" style={{minHeight: 300}}>
-        {showLogo && <img src={logo} className="App-logo" alt="logo" />}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div 
+        className="App" 
+        onClick={()=> {
+          console.log('----')
+          setShowLogo(show => !show)
+        }}
+      >
+        <header className="App-header" style={{minHeight: 300}}>
+          {showLogo && <img src={logo} className="App-logo" alt="logo" />}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
       <div style={{height:300, marginTop: 50, fontSize: 25}}>
         <button style={{width: 80, height: 50, borderRadius: 10, marginRight: 30, outline: 'none',fontSize: 25}} onClick={()=> setCount(count+1)}>+</button>
         {count}
         <button style={{width: 80, height: 50, borderRadius: 10, marginLeft: 30, outline: 'none', fontSize: 25}} onClick={()=> setCount(count-1)}>-</button>
       </div>
-    </div>
+    </>
   );
 }
 
